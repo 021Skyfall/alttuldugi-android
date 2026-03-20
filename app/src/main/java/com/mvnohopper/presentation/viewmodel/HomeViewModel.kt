b85bcd8c9c45bc8d97d422d681f8c17a26fc3c93
Jeremiah Lee <021skyfall@gmail.com>
2026-03-20 16:05:29 +0900

    fix: replace transformations with livedata map

--- app/src/main/java/com/mvnohopper/presentation/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/mvnohopper/presentation/viewmodel/HomeViewModel.kt
@@ -3,12 +3,12 @@ package com.mvnohopper.presentation.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.mvnohopper.data.database.MvnoHopperDatabase
 import com.mvnohopper.data.entity.MobileService
 import com.mvnohopper.data.repository.MobileServiceRepository
 import com.mvnohopper.domain.model.MobileServiceWithCalculations
 import com.mvnohopper.domain.usecase.CalculateDateUseCase
+import androidx.lifecycle.map
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -18,7 +18,7 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private val calculateDateUseCase = CalculateDateUseCase()
 
     val mobileServices: LiveData<List<MobileServiceWithCalculations>> =
-        Transformations.map(repository.getAll()) { items: List<MobileService> ->
+        repository.getAll().map { items: List<MobileService> ->
             items.map { calculateDateUseCase(it) }
         }
 }
